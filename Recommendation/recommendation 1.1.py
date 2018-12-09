90ad84b7a78de0a75e33ece050865d5614d8d4a9
mandarin4452 <ckdgns0130@gmail.com>
2018-12-09 17:06:25 +0900

    send recommendation table to db

--- Recommendation/recommendation 1.1.py
+++ Recommendation/recommendation 1.1.py
@@ -2,6 +2,9 @@ import mysql.connector
 import pandas as pd
 import numpy as np
 from IPython.display import display
+import time
+from datetime import date
+today = str(date.today())
 
 class DBConnection:
     def __init__(self, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME):
@@ -100,7 +103,8 @@ class user:
         return (U_SN, ID, PW, NAME, NICKNAME, ADDRESS, PHONE_NUMBER, member_dict)
 
 class table(rate, product, user):
-    def __init__(self,cursor):
+    def __init__(self,cursor, db):
+        self.db = db
         self.cursor = cursor
         self.UID, self.PID, self.RATE, self.rating_dict = rate.result(self)
         self.P_SN, self.attr1 ,self.attr2, self.attr3, self.attr4, self.attr5, self.NAME, self.PRICE, self.IMG_URL, self.PRODUCT_URL, self.VIEW, self.product_dict= product.result(self)
@@ -108,7 +112,7 @@ class table(rate, product, user):
 
     def userarray(self):
         attr_lists = []
-        print("UID : " + str(self.UID) + '\n')
+        #print("UID : " + str(self.UID) + '\n')
         for element in self.attr1:
             if element not in attr_lists:
                 attr_lists.append(element)
@@ -124,6 +128,8 @@ class table(rate, product, user):
         for element in self.attr5:
             if element not in attr_lists:
                 attr_lists.append(element)
+        #print(attr_lists)
+        #print(attr_lists.count("NULL"))
         user_blank=[]
         for i in range(len(self.U_SN)+1):
             user_blank.append(0)
@@ -131,8 +137,8 @@ class table(rate, product, user):
         #user 갯수만큼 array 생성
         U_A_dict = {x:list(tuple(user_blank)) for x in attr_lists}   #변수 차단 tuple로 변환한순간 변수x
             
-        print("Initialized U_A_dict")
-        display(pd.DataFrame(U_A_dict))
+        #print("Initialized U_A_dict")
+        #display(pd.DataFrame(U_A_dict))
         #rating to User-Attribute table
 
         for i in range(len(self.UID)):
@@ -145,13 +151,14 @@ class table(rate, product, user):
             U_A_dict[self.product_dict["attr3"][self.rating_dict['PID'][i]]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
             U_A_dict[self.product_dict["attr4"][self.rating_dict['PID'][i]]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
             U_A_dict[self.product_dict["attr5"][self.rating_dict['PID'][i]]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
-        print("Updated U_A_dict")
-        display(pd.DataFrame(U_A_dict))
+        U_A_dict['NULL'] = list(tuple(user_blank))
+        #print("Updated U_A_dict")
+        #display(pd.DataFrame(U_A_dict))
 
         #User-Product table;
         U_P_dict={x:list(tuple(user_blank)) for x in range(len(self.P_SN)+1)}
-        print("Initialized U_P_dict")
-        display(pd.DataFrame(U_P_dict))
+        #print("Initialized U_P_dict")
+        #display(pd.DataFrame(U_P_dict))
         for j in self.PID:
             for i in range(len(self.U_SN)+1):
                 U_P_dict[j][i]+=U_A_dict[self.product_dict['attr1'][j]][i]
@@ -159,19 +166,29 @@ class table(rate, product, user):
                 U_P_dict[j][i]+=U_A_dict[self.product_dict['attr3'][j]][i]
                 U_P_dict[j][i]+=U_A_dict[self.product_dict['attr4'][j]][i]
                 U_P_dict[j][i]+=U_A_dict[self.product_dict['attr5'][j]][i]
-        print("Updated U_P_dict")
-        display(pd.DataFrame(U_P_dict))
-
+                
+        #print("Updated U_P_dict")
+        #display(pd.DataFrame(U_P_dict))
+        
         #Ranking
+        
         user_rank=[]
         for i in range(len(self.U_SN)+1):
             user_rank.append([])
         for i in self.UID:
                 user_rank[i]={x:U_P_dict[x][i] for x in U_P_dict.keys()}
-
+        
         #서버로 보내는 sorted 된 product 리스트 #1에 UID
         sorted(user_rank[1],key=lambda k: user_rank[1][k],reverse=True)
-        print(sorted)
+    
+        for i in range(1, len(self.U_SN)):
+            t = sorted(user_rank[i], key = lambda k : user_rank[i][k], reverse = True)
+            input_string = "insert into recommend (U_SN, RANK1, RANK2, RANK3, RANK4, RANK5, RANK6, RANK7, RANK8, RANK9, RANK10, RANK11, RANK12, RANK13, RANK14, RANK15, RANK16, RANK17, RANK18, RANK19, RANK20, date) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            self.cursor.execute(input_string, (i,t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9],t[10],t[11],t[12],t[13],t[14],t[15],t[16],t[17],t[18],t[19],today))
+        self.db.commit()
+        print(self.cursor.rowcount, "inserted")
+        
+        
 
         
 mydb = DBConnection('localhost','root','gnsdl10','faredy_db')
@@ -181,5 +198,5 @@ mycursor = mydbconn.cursor()
 
 #pd.options.display.max_columns = None
 
-test = table(mycursor).userarray()
+test = table(mycursor, mydbconn).userarray()
 
