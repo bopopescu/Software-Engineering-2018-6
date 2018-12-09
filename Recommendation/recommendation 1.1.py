c256b66eb2390041d53c83d19a4b23a5a782a21d
mandarin4452 <ckdgns0130@gmail.com>
2018-12-09 20:43:38 +0900

    Update recommendation 1.1.py

--- Recommendation/recommendation 1.1.py
+++ Recommendation/recommendation 1.1.py
@@ -23,7 +23,6 @@ class DBConnection:
                                     passwd = self.password)
         return self.conn
 
-
 class rate:
     def __init__(self, cursor):
         self.cursor = cursor
@@ -85,22 +84,13 @@ class user:
         myresult = mycursor.fetchall()
         U_SN = []
         ID = []
-        PW = []
-        NAME = []
-        NICKNAME = []
-        ADDRESS = []
-        PHONE_NUMBER= []
+        # modified according to main DB
         for x in myresult:
             U_SN.append(x[0])
             ID.append(x[1])
-            PW.append(x[2])
-            NAME.append(x[3])
-            NICKNAME.append(x[4])
-            ADDRESS.append(x[5])
-            PHONE_NUMBER.append(x[6])
-
-        member_dict = {"U_SN":U_SN,"ID":ID,"PW":PW,"NAME":NAME,"NICKNAME":NICKNAME,"ADDRESS":ADDRESS,"PHONE_NUMBER":PHONE_NUMBER}
-        return (U_SN, ID, PW, NAME, NICKNAME, ADDRESS, PHONE_NUMBER, member_dict)
+            
+        member_dict = {"U_SN":U_SN,"ID":ID}
+        return (U_SN, ID, member_dict)
 
 class table(rate, product, user):
     def __init__(self,cursor, db):
@@ -108,7 +98,7 @@ class table(rate, product, user):
         self.cursor = cursor
         self.UID, self.PID, self.RATE, self.rating_dict = rate.result(self)
         self.P_SN, self.attr1 ,self.attr2, self.attr3, self.attr4, self.attr5, self.NAME, self.PRICE, self.IMG_URL, self.PRODUCT_URL, self.VIEW, self.product_dict= product.result(self)
-        self.U_SN, self.ID, self.PW, self.NAME2, self.NICKNAME, self.ADDRESS, self.PHONE_NUMBER, self.member_dict = user.result(self)
+        self.U_SN, self.ID, self.member_dict = user.result(self)
 
     def userarray(self):
         attr_lists = []
@@ -146,11 +136,11 @@ class table(rate, product, user):
             #rating_dict['PID'][0]=0번째 PID
             #rating_dict['RATE'][0]=0번째 RATING
             #product_dict["TYPE"][i]에 해당하는 행에 ++를 한다
-            U_A_dict[self.product_dict["attr1"][self.rating_dict['PID'][i]]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
-            U_A_dict[self.product_dict["attr2"][self.rating_dict['PID'][i]]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
-            U_A_dict[self.product_dict["attr3"][self.rating_dict['PID'][i]]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
-            U_A_dict[self.product_dict["attr4"][self.rating_dict['PID'][i]]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
-            U_A_dict[self.product_dict["attr5"][self.rating_dict['PID'][i]]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
+            U_A_dict[self.product_dict["attr1"][self.rating_dict['PID'][i]-1]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
+            U_A_dict[self.product_dict["attr2"][self.rating_dict['PID'][i]-1]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
+            U_A_dict[self.product_dict["attr3"][self.rating_dict['PID'][i]-1]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
+            U_A_dict[self.product_dict["attr4"][self.rating_dict['PID'][i]-1]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
+            U_A_dict[self.product_dict["attr5"][self.rating_dict['PID'][i]-1]][self.rating_dict['UID'][i]]+=self.rating_dict['RATE'][i]
         U_A_dict['NULL'] = list(tuple(user_blank))
         #print("Updated U_A_dict")
         #display(pd.DataFrame(U_A_dict))
@@ -159,16 +149,16 @@ class table(rate, product, user):
         U_P_dict={x:list(tuple(user_blank)) for x in range(len(self.P_SN)+1)}
         #print("Initialized U_P_dict")
         #display(pd.DataFrame(U_P_dict))
-        for j in self.PID:
+        for j in range(1,len(self.P_SN) + 1):
             for i in range(len(self.U_SN)+1):
-                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr1'][j]][i]
-                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr2'][j]][i]
-                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr3'][j]][i]
-                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr4'][j]][i]
-                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr5'][j]][i]
+                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr1'][j-1]][i]
+                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr2'][j-1]][i]
+                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr3'][j-1]][i]
+                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr4'][j-1]][i]
+                U_P_dict[j][i]+=U_A_dict[self.product_dict['attr5'][j-1]][i]
                 
         #print("Updated U_P_dict")
-        #display(pd.DataFrame(U_P_dict))
+        display(pd.DataFrame(U_P_dict))
         
         #Ranking
         
@@ -179,24 +169,25 @@ class table(rate, product, user):
                 user_rank[i]={x:U_P_dict[x][i] for x in U_P_dict.keys()}
         
         #서버로 보내는 sorted 된 product 리스트 #1에 UID
-        sorted(user_rank[1],key=lambda k: user_rank[1][k],reverse=True)
-    
+        #sorted(user_rank[1],key=lambda k: user_rank[1][k],reverse=True)
+        # Sending recommend table to server
         for i in range(1, len(self.U_SN)):
-            t = sorted(user_rank[i], key = lambda k : user_rank[i][k], reverse = True)
-            input_string = "insert into recommend (U_SN, RANK1, RANK2, RANK3, RANK4, RANK5, RANK6, RANK7, RANK8, RANK9, RANK10, RANK11, RANK12, RANK13, RANK14, RANK15, RANK16, RANK17, RANK18, RANK19, RANK20, date) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-            self.cursor.execute(input_string, (i,t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9],t[10],t[11],t[12],t[13],t[14],t[15],t[16],t[17],t[18],t[19],today))
+            t = sorted(user_rank[i], key = lambda k : user_rank[i][k], reverse = False)
+            for j in user_rank[i]:
+                if user_rank[i][j] != 0:
+                    input_string = "insert into recommend (ID, RANK, date) values (%s, %s, %s)"
+                    self.cursor.execute(input_string, (self.ID[i],user_rank[i][j],today))
         self.db.commit()
         print(self.cursor.rowcount, "inserted")
         
         
 
         
-mydb = DBConnection('localhost','root','gnsdl10','faredy_db')
+mydb = DBConnection('localhost','root','gnsdl10','faredy_db').get_conn()
 # change host/name/passwd/DB according to Server
-mydbconn = mydb.get_conn()
-mycursor = mydbconn.cursor()
+mycursor = mydb.cursor()
 
 #pd.options.display.max_columns = None
 
-test = table(mycursor, mydbconn).userarray()
+test = table(mycursor, mydb).userarray()
 
